ccd404ea5ba29b2f1e6fea196fbcc696f090b5d9
Armin Nazari <anphoenix1999@gmail.com>
2019-09-11 14:24:17 +0430

    create expense, income, custom user models for main app and register admin page

---
+++ main/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    wallet = models.BigIntegerField(default=0)
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=40)
+    value = models.BigIntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=40)
+    value = models.BigIntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
