4a89c6cd9fee5c7e48be5c9003832c1961df06c6
Armin Nazari <anphoenix1999@gmail.com>
2019-09-11 17:51:33 +0430

    change related name for income and expense

--- main/models.py
+++ main/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     wallet = models.BigIntegerField(default=0)
 
+    def __str__(self):
+        return self.username
+
 
 class Expense(models.Model):
     text = models.CharField(max_length=40)
     value = models.BigIntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser,
+                             null=True,
+                             on_delete=models.CASCADE,
+                             related_name='expenses')
 
     def __str__(self):
         return self.text
@@ -21,7 +27,10 @@ class Income(models.Model):
     text = models.CharField(max_length=40)
     value = models.BigIntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser,
+                             null=True,
+                             on_delete=models.CASCADE,
+                             related_name='incomes')
 
     def __str__(self):
         return self.text
