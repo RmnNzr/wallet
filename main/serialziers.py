3f7a038823e66899a3767b35bee529621a18df62
Armin Nazari <anphoenix1999@gmail.com>
2019-09-14 14:56:56 +0430

    add query for expense and income to return last month of them

--- main/serialziers.py
+++ main/serialziers.py
@@ -1,13 +1,18 @@
 from rest_framework.serializers import Serializer, ModelSerializer
 from rest_framework import serializers
 from .models import Expense, Income, CustomUser
+from datetime import datetime, timedelta
+
+query_last_month = [(datetime.now() - timedelta(days=30)), datetime.now()]
 
 
 class CustomUserSerializer(ModelSerializer):
-    expenses = serializers.StringRelatedField(many=True,
-                                              read_only=True)
-    incomes = serializers.StringRelatedField(many=True,
-                                             read_only=True)
+    expenses = serializers.SlugRelatedField(many=True,
+                                            slug_field='text',
+                                            queryset=Expense.objects.filter(date__range=query_last_month))
+    incomes = serializers.SlugRelatedField(many=True,
+                                           slug_field='text',
+                                           queryset=Income.objects.filter(date__range=query_last_month))
 
     class Meta:
         model = CustomUser
